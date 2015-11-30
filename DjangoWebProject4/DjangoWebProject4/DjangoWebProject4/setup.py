0aef77e221ec5348bbae9e3170f5d35f6514d0e2
travon-speller <travon.speller@bison.howard.edu>
2015-11-30 14:39:32 -0500

    issues with view module

---
+++ DjangoWebProject4/DjangoWebProject4/DjangoWebProject4/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(
+    name='django-scheduler',
+    version='0.7.6',
+    description='A calendaring app for Django.',
+    author='Leonardo Lazzaro',
+    author_email='lazzaroleonardo@gmail.com',
+    url='https://github.com/llazzaro/django-scheduler',
+    packages=[
+        'schedule',
+        'schedule.conf',
+        'schedule.feeds',
+        'schedule.management',
+        'schedule.management.commands',
+        'schedule.models',
+        'schedule.migrations',
+        'schedule.templatetags',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=['Development Status :: 4 - Beta',
+                 'Environment :: Web Environment',
+                 'Framework :: Django',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Utilities'],
+    install_requires=[
+        'Django>=1.7',
+        'python-dateutil>=2.1',
+        'pytz>=2013.9',
+        'six>=1.3.0',
+        'icalendar>=3.8.4',
+        'django-annoying>=0.8.0',
+        'future>=0.14.2',
+        'django-bower==5.0.4',
+        'django-compressor==1.5',
+    ],
+    license='BSD',
+    test_suite='runtests.runtests',
+)
---
+++ DjangoWebProject4/DjangoWebProject4/env/Scripts/futurize-script.py
@@ -0,0 +1,10 @@
+#!c:\users\tray\documents\visual studio 2015\Projects\DjangoWebProject4\DjangoWebProject4\env\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'future==0.15.2','console_scripts','futurize'
+__requires__ = 'future==0.15.2'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('future==0.15.2', 'console_scripts', 'futurize')()
+    )
